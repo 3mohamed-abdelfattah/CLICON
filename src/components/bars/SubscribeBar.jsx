f80e153c7b6113a3ef335ee3ee9cb585746bdf9e
MohamedMohamed <m01126165016@icloud.com>
2024-09-15 01:45:18 +0300

    All Bar Done

--- src/components/bars/SubscribeBar.jsx
+++ src/components/bars/SubscribeBar.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { CompanyLogo, SubscribeButtonIcon } from '../../utils/icons.util'
 
 export const SubscribeBar = () => {
     return (
-        <div>SubscribeBar</div>
+        <main className='flex flex-col justify-center items-center h-[452px] bg-primaryBackground'>
+            <p className='font-semibold text-white text-3xl leading-10'>Subscribe to our newsletter</p>
+            <p className='max-w-[536px] text-center text-white/60 text-base leading-6 p-3'>Praesent fringilla erat a lacinia egestas. Donec vehicula tempor libero et cursus. Donec non quam urna. Quisque vitae porta ipsum.</p>
+            <span className='flex w-[624px] bg-white px-4 py-[12px] rounded-sm gap-2 shadow-xl m-8'>
+                <input
+                    placeholder='Email address'
+                    className='text-sm font-normal leading-5 text-secondaryBackground w-full'
+                />
+                <span className='cursor-pointer'>
+                    <SubscribeButtonIcon />
+                </span>
+            </span>
+            <hr className='border-gray-500' />
+            <CompanyLogo />
+        </main>
     )
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import { DiscountBar, Header, Footer } from '@/components'
+import { DiscountBar, Header, Footer, SubscribeBar } from '@/components'
 
 export const HomePage = () => {
 
-    const styles = {
-        mainContainer: `flex flex-col justify-center items-center h-screen bg-[#D6EFD8]`
-    }
-
     return (
-        <main className={styles.mainContainer}>
+        <main>
             <DiscountBar />
             <Header />
+            <SubscribeBar />
             <Footer />
         </main>
     )
