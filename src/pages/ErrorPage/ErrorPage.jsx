42294fe0afad718600a8393303422ac413287f8b
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2025-01-02 18:17:32 +0200

    Clean ErrorPage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,29 +1,36 @@
-import React, { Fragment } from 'react'
-import { Link } from 'react-router-dom'
-import { Header, Footer } from '@/components'
-//Icons
-import { GoBack, GoHome } from '@/utils/icons.util'
-//images
-import ErrorLogo from '@/assets/images/error.png'
+import { Link } from 'react-router-dom';
+// Components
+import { Header, Footer } from '@/components';
+// Icons
+import * as Icons from '@/utils/icons.util';
+// Images
+import ErrorLogo from '@/assets/images/error.png';
 
 export const ErrorPage = () => {
     return (
-        <Fragment>
+        <>
+            {/* Header component */}
             <Header />
-            <div className='flex flex-col justify-center items-center gap-5 mb-16'>
-                <img className='max-w-[500px]' src={ErrorLogo} alt='ADS' />
-                <p className='font-semibold text-4xl text-center leading-[44px]'>404, Page not founds</p>
-                <p className='max-w-[536px] text-[#475156] text-center leading-6'>Something went wrong. It’s look that your requested could not be found. It’s look like the link is broken or the page is removed.</p>
+            <main className='flex flex-col justify-center items-center gap-5 mb-16'>
+                {/* Error image */}
+                <img className='max-w-[500px]' src={ErrorLogo} alt='Error' />
+                {/* Error message */}
+                <h1 className='font-semibold text-4xl text-center leading-[44px]'>404, Page not found</h1>
+                <p className='max-w-[536px] text-[#475156] text-center leading-6'>
+                    Something went wrong. It looks like the page you requested could not be found. The link might be broken or the page is being updated.
+                </p>
+                {/* Navigation links */}
                 <div className='flex gap-4'>
-                    <Link to='./home'>
-                        <GoBack />
+                    <Link to='/home' className='hover:shadow hover:scale-105 transition-all' aria-label="Go Back">
+                        <Icons.GoBack />
                     </Link>
-                    <Link to='./home'>
-                        <GoHome />
+                    <Link to='/home' className='hover:shadow hover:scale-105 transition-all' aria-label="Go Home">
+                        <Icons.GoHome />
                     </Link>
                 </div>
-            </div>
+            </main>
+            {/* Footer component */}
             <Footer />
-        </Fragment>
-    )
-}
+        </>
+    );
+};
